0361b2c21cff4df3180bb9092e1411e202fdb8b2
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-03-31 21:15:16 +0900

    update getAllTasks & getSingleTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,13 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req,res) => {
-    res.send("get all task");
-}
+const getAllTasks = async (req,res) => {
+    try {
+        const getAllTask = await Task.find({});
+        res.status(200).json(getAllTask);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
 
 const createTask = async (req,res) => {
     try {
@@ -11,19 +16,27 @@ const createTask = async (req,res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-}
+};
 
-const getSingleTask = (req,res) => {
-    res.send("get task");
-}
+const getSingleTask = async (req,res) => {
+    try {
+        const getSingleTask = await Task.findOne({_id: req.params.id});
+        if(!getSingleTask) {
+            return res.status(404).json(`_id${req.params.id} does not exist`);
+        }
+        res.status(200).json(getSingleTask);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
 
 const updateTask = (req,res) => {
     res.send("update task");
-}
+};
 
 const deleteTask = (req,res) => {
     res.send("delete task");
-}
+};
 
 module.exports = {
     getAllTasks,
