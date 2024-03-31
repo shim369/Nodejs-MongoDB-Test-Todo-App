2810b383c5313a72ee1988bb810dbb1b456f667c
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-03-31 17:30:09 +0900

    add connectDB

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const taskRoute = require("./routes/tasks")
+const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config();
 
 const port = 3000;
 
 app.use("/api/tasks", taskRoute);
+
+connectDB(process.env.MONGO_URI);
 
 app.listen(port, console.log("server ok!"))
